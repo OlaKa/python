44f687aa3a3f507561dc562a92ddf8843d64ccdf
Ola Karlsson <ola.karlsson@bahnhof.se>
2016-03-31 00:10:39 +0200

    First draft

---
+++ src/inputOutString.py
@@ -0,0 +1,14 @@
+class InputOutString:
+    def __init__(self):
+        self.s = ""
+
+    def getName(self):
+        self.s=raw_input("What is your name?:")
+        
+
+    def printString(self,name):
+        print "No Python code here yet! %s" %name
+
+strObj = InputOutString()
+strObj.getName()
+strObj.printString(strObj.s)
