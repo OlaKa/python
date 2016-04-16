e12f97e8e204a0d11cdae0ce453139499447053b
Ola Karlsson <ola.karlsson@bahnhof.se>
2016-04-17 01:21:25 +0200

    First draft

---
+++ src/xmlminidom.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python
+#
+#Minidom is used to parse xml files
+#This is a typical use. Used in real life. 
+
+from xml.dom import minidom
+import re
+
+xmlstring="""... Lisa Test, 1992/03/07, I like long
+... walks of the beach, watching sun-sets,
+... and listening to slow jazz
+<?xml version="1.0"?>
+<!DOCTYPE PARTS SYSTEM "parts.dtd">
+<?xml-stylesheet type="text/css" href="xmlpartsstyle.css"?>
+<PARTS>
+   <TITLE>Computer Parts</TITLE>
+   <PART>
+      <ITEM>Motherboard</ITEM>
+      <MANUFACTURER>ASUS</MANUFACTURER>
+      <MODEL>P3B-F</MODEL>
+      <COST> 123.00</COST>
+   </PART>
+   <PART>
+      <ITEM>Video Card</ITEM>
+      <MANUFACTURER>ATI</MANUFACTURER>
+      <MODEL>All-in-Wonder Pro</MODEL>
+      <COST> 160.00</COST>
+   </PART>
+   <PART>
+      <ITEM>Sound Card</ITEM>
+      <MANUFACTURER>Creative Labs</MANUFACTURER>
+      <MODEL>Sound Blaster Live</MODEL>
+      <COST> 80.00</COST>
+   </PART>
+   <PART>
+      <ITEM inch Monitor</ITEM>
+      <MANUFACTURER>LG Electronics</MANUFACTURER>
+      <MODEL> 995E</MODEL>
+      <COST> 290.00</COST>
+   </PART>
+</PARTS>"""
+
+l=[]
+
+#Clean file to use only xml code otherwise minidom wont work
+for line in xmlstring.split('\n'):
+    l.append(line.strip())
+    newxml='\n'.join(l)
+    newxml=re.search(r'<..*>$',newxml)
+    if newxml:
+        print newxml.group(0)
+print newxml
