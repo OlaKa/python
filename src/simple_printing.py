7a3f5f6e39b26fd47c3e660ad6bcd5708834a6b0
Ola Karlsson <ola.karlsson@bahnhof.se>
2017-07-10 00:00:29 +0200

    Added list handling from file

--- src/simple_printing.py
+++ src/simple_printing.py
@@ -25,4 +25,21 @@ with open("C:\\Users\\Ola\\Documents\\sample.txt",'r') as jabber:
      lines = jabber.readlines()
 for line in lines[::-1]:
      print(line, end='')
+  
+# read from file and remove \n
+
+list = ['Ola','Karlsson','Melania', 'Trump']
+conc_list = []
+with open('names.txt','w') as name_list:
+    for names in list:
+        print(names, file=name_list)
+
+with open('names.txt', 'r') as file:
+    for line in file:
+        conc_list.append(line)
+
+
+strip_list = [item.strip() for item in conc_list]
+
+print(strip_list)
      
